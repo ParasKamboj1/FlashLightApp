cc6ddb37d4fd8d377b54ca17dd353e60d1a461ca
Paras Kamboj <paras1234567899@gmail.com>
2024-03-10 05:29:34 +0530

    hello this is the last commit

--- app/src/main/java/com/example/flashlight/MainActivity.java
+++ app/src/main/java/com/example/flashlight/MainActivity.java
@@ -9,31 +9,28 @@ import android.hardware.camera2.CameraManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+
     ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
+
         image = findViewById(R.id.imageView);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(button.getText().toString().equals("ON")){
-                    button.setText("OFF");
-                    image.setImageResource(R.drawable.flashlighton);
+                if(image.getDrawable().getConstantState().equals(getResources().getDrawable(R.drawable.closed).getConstantState())){
+                    image.setImageResource(R.drawable.on);
                     changeLightState(true);
                 }
                 else{
-                    button.setText("ON");
-                    image.setImageResource(R.drawable.flashlightoff);
+                    image.setImageResource(R.drawable.closed);
                     changeLightState(false);
                 }
             }
@@ -58,8 +55,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        button.setText("ON");
-        image.setImageResource(R.drawable.flashlightoff);
+        image.setImageResource(R.drawable.closed);
     }
 
 }
